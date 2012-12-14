1674c6c797f4a09a385071ec204e1331186be93c
zaufi <i.zaufi@gmail.com>
2012-12-14 09:50:02 +0400

    improve clang detector

--- cmake/modules/FindClangCLibs.cmake
+++ cmake/modules/FindClangCLibs.cmake
@@ -5,35 +5,38 @@
 #
 # kate: hl cmake;
 
+# NOTE In gentoo clang library placed into /usr/lib64/llvm,
+# so to find it `llvm-config` required
+# But in Ubuntu 12.10 libclang.so placed into /usr/lib...
 find_program(
     LLVM_CONFIG_EXECUTABLE
-    llvm-config                                             # My gentoo system has this name
-    llvm-config-3.1                                         # ???
-    llvm-config-3.0                                         # Ubuntu 12.04 (quantal)
-    REQUIRED
+    llvm-config
   )
-message(STATUS "Found LLVM configuration tool: ${LLVM_CONFIG_EXECUTABLE}")
+if (LLVM_CONFIG_EXECUTABLE)
+    message(STATUS "Found LLVM configuration tool: ${LLVM_CONFIG_EXECUTABLE}")
 
-# Get LLVM CXX flags
-execute_process(
-    COMMAND ${LLVM_CONFIG_EXECUTABLE} --cxxflags
-    OUTPUT_VARIABLE LLVM_CXXFLAGS
-    OUTPUT_STRIP_TRAILING_WHITESPACE
-  )
-# Remove undesirable flags
-string(REPLACE "-DNDEBUG" "" LLVM_CXXFLAGS "${LLVM_CXXFLAGS}")
-string(
-    REPLACE "-fno-exceptions" ""
-    LLVM_CXXFLAGS
-    "${LLVM_CXXFLAGS}"
-  )
+    # Get LLVM CXX flags
+    execute_process(
+        COMMAND ${LLVM_CONFIG_EXECUTABLE} --cxxflags
+        OUTPUT_VARIABLE LLVM_CXXFLAGS
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+      )
+    # Remove undesirable flags
+    string(REPLACE "-DNDEBUG" "" LLVM_CXXFLAGS "${LLVM_CXXFLAGS}")
+    string(
+        REPLACE "-fno-exceptions" ""
+        LLVM_CXXFLAGS
+        "${LLVM_CXXFLAGS}"
+      )
 
-# Get LLVM library dir
-execute_process(
-    COMMAND ${LLVM_CONFIG_EXECUTABLE} --libdir
-    OUTPUT_VARIABLE LLVM_LIBDIR
-    OUTPUT_STRIP_TRAILING_WHITESPACE
-  )
+    # Get LLVM library dir
+    execute_process(
+        COMMAND ${LLVM_CONFIG_EXECUTABLE} --libdir
+        OUTPUT_VARIABLE LLVM_LIBDIR
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+      )
+endif()
+
 # Try to find libclang.so
 find_library(
     CLANG_C_LIBRARIES
@@ -52,12 +55,19 @@ try_run(
   )
 # Extract clang version. In my system this string look like:
 # "clang version 3.1 (branches/release_31)"
-string(REGEX REPLACE ".*([23][^ ]+) .*" "\\1" CLANG_C_LIB_VERSION "${_clang_get_version_run_output}")
+# In Ubuntu 12.10 it looks like:
+# "Ubuntu clang version 3.0-6ubuntu3 (tags/RELEASE_30/final) (based on LLVM 3.0)"
+string(
+    REGEX
+    REPLACE ".*clang version ([23][^ \\-]+)[ \\-].*" "\\1"
+    CLANG_C_LIB_VERSION
+    "${_clang_get_version_run_output}"
+  )
 
 message(STATUS "Found Clang C API version ${CLANG_C_LIB_VERSION}")
 
 find_package_handle_standard_args(
     CLANG_C_LIBS
-    REQUIRED_VARS LLVM_CXXFLAGS CLANG_C_LIBRARIES
+    REQUIRED_VARS CLANG_C_LIBRARIES
     VERSION_VAR CLANG_C_LIB_VERSION
   )
