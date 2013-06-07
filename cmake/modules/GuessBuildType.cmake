bebc17ca3318623305ffacf4ea9cc639615c63a3
zaufi <i.zaufi@gmail.com>
2013-06-08 00:07:43 +0400

    update chewy modules

--- cmake/modules/GuessBuildType.cmake
+++ cmake/modules/GuessBuildType.cmake
@@ -1,10 +1,23 @@
+# - Guess appropriate value for CMAKE_BUILD_TYPE if latter is not set
+#
+# If no CMAKE_BUILD_TYPE provided to `cmake` try to guess it.
+# The end-user, who get a tarball, likely wants
+# to build a release, but developers (who has VCS dirs on top of a
+# source tree), likely wants to build a debug version...
+#
+
+#=============================================================================
 # Copyright 2012 by Alex Turbov <i.zaufi@gmail.com>
 #
-# Setup build type. If no CMAKE_BUILD_TYPE provided to `cmake`
-# try to guess it. The end-user, who get a tarball, likely wants
-# to build a release, but developers (who has VCS dirs on top of a
-# source tree), more likely want to build a debug version...
+# Distributed under the OSI-approved BSD License (the "License");
+# see accompanying file LICENSE for details.
 #
+# This software is distributed WITHOUT ANY WARRANTY; without even the
+# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the License for more information.
+#=============================================================================
+# (To distribute this file outside of this repository, substitute the full
+#  License text for the above reference.)
 
 # If CMAKE_BUILD_TYPE is not set, check for VCS files
 if (NOT CMAKE_BUILD_TYPE)
@@ -19,5 +32,5 @@ message(STATUS "Configuration type choosen: ${CMAKE_BUILD_TYPE}")
 # kate: hl cmake;
 # X-Chewy-RepoBase: https://raw.github.com/mutanabbi/chewy-cmake-rep/master/
 # X-Chewy-Path: GuessBuildType.cmake
-# X-Chewy-Version: 1.2
-# X-Chewy-Description: Helper to install Python scripts
+# X-Chewy-Version: 1.4
+# X-Chewy-Description: Guess build type if not specified explicitly
--- cmake/modules/UsePCHFile.cmake
+++ cmake/modules/UsePCHFile.cmake
@@ -1,8 +1,33 @@
-# Copyright (c) 2012-2013 by Alex Turbov
+# - Function to render a header file that can be used as PCH
 #
-# Macro to collect most used imported (i.e. not from this project) headers
-# and generate a file w/ #include directives for top of them
+# `use_pch_file()` will add a target (named 'update-pch-header') to collect
+# most used imported (i.e. not from this project) headers
+# and generate a file w/ #include directives for all of them.
 #
+#   use_pch_file(PCH_FILE <file>
+#       EXCLUDE_DIRS dir1 [... dirN]
+#       EXCLUDE_HEADERS file1 [... fileN]
+#     )
+#
+# Parameters are:
+#
+#   PCH_FILE        -- an output filename to be generated
+#   EXCLUDE_DIRS    -- a list of directories to exclude from scan
+#   EXCLUDE_HEADERS -- a list of header files to exclude from result PCH file
+#
+
+#=============================================================================
+# Copyright 2012-2013 by Alex Turbov <i.zaufi@gmail.com>
+#
+# Distributed under the OSI-approved BSD License (the "License");
+# see accompanying file LICENSE for details.
+#
+# This software is distributed WITHOUT ANY WARRANTY; without even the
+# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the License for more information.
+#=============================================================================
+# (To distribute this file outside of this repository, substitute the full
+#  License text for the above reference.)
 
 set(_USE_PHC_FILE_MODULE_BASE_DIR "${CMAKE_CURRENT_LIST_DIR}")
 
@@ -44,7 +69,7 @@ function(use_pch_file)
 
     add_custom_target(
         update-pch-header
-        COMMAND ${CMAKE_COMMAND} -P ${_USE_PHC_FILE_MODULE_BASE_DIR}/PreparePCHHeader.cmake
+        COMMAND ${CMAKE_COMMAND} -P ${CMAKE_BINARY_DIR}/PreparePCHHeader.cmake
         MAIN_DEPENDENCY ${CMAKE_BINARY_DIR}/PreparePCHHeader.cmake
         COMMENT "Updating ${use_pch_file_PCH_FILE}"
       )
@@ -54,7 +79,7 @@ endfunction()
 # kate: hl cmake;
 # X-Chewy-RepoBase: https://raw.github.com/mutanabbi/chewy-cmake-rep/master/
 # X-Chewy-Path: UsePCHFile.cmake
-# X-Chewy-Version: 2.6
+# X-Chewy-Version: 2.7
 # X-Chewy-Description: Add Precompiled Header Support
 # X-Chewy-AddonFile: PreparePCHHeader.cmake.in
 # X-Chewy-AddonFile: pch-template.h.in
